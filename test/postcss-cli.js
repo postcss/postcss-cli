0f535c5c6edeab544d8073c6d572494e88ad06e5
RyanZim <opensrc@ryanzim.com>
2017-01-04 20:16:13 -0500

    Add test for --dir

--- test/postcss-cli.js
+++ test/postcss-cli.js
@@ -1,6 +1,7 @@
 import test from 'ava'
 import run from './helpers/run-cli.js'
 import fs from 'fs-promise'
+import path from 'path'
 import tmp from 'tempfile'
 
 function read (path) {
@@ -12,4 +13,12 @@ test('works without plugins or config', async function (t) {
   var { error } = await run(['test/fixtures/a-red.css', '-o', out])
   t.ifError(error)
   t.is(await read(out), await read('test/fixtures/a-red.css'))
+})
+
+test('--dir works', async function (t) {
+  var outDir = tmp()
+  var { error } = await run(['test/fixtures/a-red.css', 'test/fixtures/a-blue.css', '--dir', outDir])
+  t.ifError(error)
+  t.is(await read(path.join(outDir, 'a-red.css')), await read('test/fixtures/a-red.css'))
+  t.is(await read(path.join(outDir, 'a-blue.css')), await read('test/fixtures/a-blue.css'))
 })

eba3cb4999421a6659109ccb4512a0263d16e72e
RyanZim <opensrc@ryanzim.com>
2017-01-07 09:29:17 -0500

    Add test for common config support

--- test/helpers/run-cli.js
+++ test/helpers/run-cli.js
@@ -1,9 +1,10 @@
 'use strict'
+const path = require('path')
 const execFile = require('child_process').execFile
 
-module.exports = function (args) {
+module.exports = function (args, cwd) {
   return new Promise(function (resolve) {
-    execFile('bin/postcss', args, function (error, stdout, stderr) {
+    execFile(path.resolve('bin/postcss'), args, {cwd}, function (error, stdout, stderr) {
       resolve({
         code: error && error.code ? error.code : 0,
         error,

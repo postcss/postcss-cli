b183f62a870d97b1a935a3c0de15c0fab927b17c
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2017-01-14 02:47:54 +0100

    test(errors): change err message for output

--- test/errors.js
+++ test/errors.js
@@ -1,10 +1,11 @@
 import test from 'ava'
+
 import run from './helpers/run-cli.js'
 
 test('errors when no --output, --dir, or --replace option is passed', (t) => {
-  return run(['test/fixtures/a-red.css'])
-  .then(({error, code}) => {
-    t.is(code, 1, 'expected non-zero error code')
-    t.regex(error, /Must pass --output, --dir, or --replace option/)
-  })
+  return run([ 'test/fixtures/a-red.css' ])
+    .then(({ error, code }) => {
+      t.is(code, 1, 'expected non-zero error code')
+      t.regex(error, /No Output specified, either --output, --dir, or --replace option must be passed/)
+    })
 })

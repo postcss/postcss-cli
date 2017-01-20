6a80c71752b4d43d5a8034d039337e2d1f5deba5
RyanZim <opensrc@ryanzim.com>
2017-01-20 14:41:20 -0500

    Read from stdin if no files are passed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const chalk = require('chalk')
 const spinner = require('ora')()
 const globber = require('globby')
 const chokidar = require('chokidar')
+const getStdin = require('get-stdin')
 
 const postcss = require('postcss')
 const postcssrc = require('postcss-load-config')
@@ -78,17 +79,22 @@ console.warn(chalk.bold.red(logo))
 
 spinner.text = `Loading Config`
 spinner.start()
-Promise.all([ globber(input), getConfig({}, argv.config) ])
-  .then((arr) => {
-    // Until parameter destructuring is supported
-    let files = arr[0]
-
-    if (!files || !files.length) throw new Error('You must pass a list of files to parse')
-    if (files.length > 1 && argv.output) throw new Error('Must use --dir or --replace with multiple input files')
-
+getConfig({}, argv.config)
+  .then(() => {
     spinner.succeed()
 
-    return Promise.all(files.map(file => processCSS(file)))
+    if (input && input.length) return globber(input)
+    // Else, read from stdin:
+    if (!argv.output) throw new Error('Must specify --output when reading from stdin')
+    if (argv.watch) throw new Error('Cannot run in watch mode when reading from stdin')
+    console.warn('No files passed, reading from stdin')
+    return ['-']
+  })
+  .then(files => {
+    if (!files || !files.length) throw new Error('You must pass a valid list of files to parse')
+    if (files.length > 1 && argv.output) throw new Error('Must use --dir or --replace with multiple input files')
+
+    return processFiles(files)
   })
   .then(function () {
     if (argv.watch) {
@@ -103,12 +109,12 @@ Promise.all([ globber(input), getConfig({}, argv.config) ])
       .on('change', (file) => {
         if (file === config.file) {
           return Promise.all([globber(input), getConfig()])
-          .then(arr => Promise.all(arr[0].map(file => processCSS(file))))
+          .then(arr => processFiles(arr[0]))
           .catch(error)
         }
         spinner.text = `Processing ${chalk.green(`${file}`)}`
 
-        getConfig().then(() => processCSS(file, watcher))
+        getConfig().then(() => processFiles(file, watcher))
         .then(() => {
           spinner.text = 'Waiting for file changes...'
           spinner.start()
@@ -119,22 +125,17 @@ Promise.all([ globber(input), getConfig({}, argv.config) ])
   })
   .catch(error)
 
-function processCSS (file, watcher) {
-  spinner.text = `Processing ${file}`
-  spinner.start()
-
+function processCSS (css, filename, watcher) {
   let options = Object.assign(
     {
-      from: file,
-      to: output || (argv.replace ? file : path.join(dir, path.basename(file)))
+      from: filename,
+      to: output || (argv.replace ? filename : path.join(dir, path.basename(filename)))
     },
     config.options
   )
-
   options.to = path.resolve(options.to)
 
-  return fs.readFile(file)
-    .then(css => postcss(config.plugins).process(css, options))
+  return postcss(config.plugins).process(css, options)
     .then((result) => {
       if (watcher) {
         result.messages
@@ -150,10 +151,22 @@ function processCSS (file, watcher) {
       }
       return Promise.all(tasks)
         .then(() => {
-          spinner.succeed()
           return result
         })
     })
+}
+
+function processFiles (files, watcher) {
+  if (typeof files === 'string') files = [files]
+  return Promise.all(files.map(file => {
+    // Read from stdin
+    if (file === '-') {
+      return getStdin()
+      .then(css => processCSS(css, undefined, watcher))
+    }
+    return fs.readFile(file)
+    .then(css => processCSS(css, file, watcher))
+  }))
 }
 
 function getConfig (ctx, path) {

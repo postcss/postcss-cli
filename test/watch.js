cd2a5d8121759197799e066576edbedc134bb07d
RyanZim <opensrc@ryanzim.com>
2017-01-21 17:19:23 -0500

    Get dependency watching working, add test

--- test/watch.js
+++ test/watch.js
@@ -147,3 +147,73 @@ test.cb('--watch watches postcss.config.js', function (t) {
   // Timeout:
   setTimeout(() => t.end('test timeout'), 50000)
 })
+
+test.cb('--watch watches dependencies', function (t) {
+  var cp
+
+  t.plan(2)
+
+  createEnv('', ['*a-red.css'])
+  .then(dir => {
+    // Init watcher:
+    var watcher = chokidar.watch('.', {
+      cwd: dir,
+      ignoreInitial: true,
+      awaitWriteFinish: true
+    })
+
+    // On the first output:
+    watcher.on('add', p => {
+      // Assert, then change the source file
+      if (p === 'out.css') {
+        isEqual(p, 'test/fixtures/a-red.css')
+        .then(() => read('test/fixtures/a-blue.css'))
+        .then(css => fs.writeFile(path.join(dir, 'a-red.css'), css))
+        .catch(done)
+      }
+    })
+
+    // When the change is picked up:
+    watcher.on('change', p => {
+      if (p === 'out.css') {
+        isEqual(p, 'test/fixtures/a-blue.css')
+        .then(() => done())
+        .catch(done)
+      }
+    })
+
+    // Start postcss-cli:
+    watcher.on('ready', () => {
+      cp = execFile(
+        path.resolve('bin/postcss'),
+        [
+          'imports-a-red.css',
+          '-o', 'out.css',
+          '-u', 'postcss-import',
+          '-w',
+          '--no-map'
+        ],
+        {cwd: dir}
+      )
+      cp.on('error', t.end)
+      cp.on('exit', code => { if (code) t.end(code) })
+    })
+
+    // Helper functions:
+    function isEqual (p, expected) {
+      return Promise.all([read(path.join(dir, p)), read(expected)])
+      .then(([a, e]) => t.is(a, e))
+    }
+
+    function done (err) {
+      try {
+        cp.kill()
+      } catch (e) {}
+      t.end(err)
+    }
+  })
+  .catch(t.end)
+
+  // Timeout:
+  setTimeout(() => t.end('test timeout'), 50000)
+})

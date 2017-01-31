7ddd32c1b7ea6d826d79f4e221e6dffbf614b2f6
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2017-01-31 14:42:43 +0100

    reactor(index): mv get-dependency-messages

--- index.js
+++ index.js
@@ -3,17 +3,16 @@
 const fs = require('fs-promise')
 const path = require('path')
 
+const stdin = require('get-stdin')
+const stream = require('stream')
+
 const chalk = require('chalk')
 const ora = require('ora')
-const spinner = ora()
 const globber = require('globby')
 const chokidar = require('chokidar')
-const getStdin = require('get-stdin')
 
 const postcss = require('postcss')
 const postcssrc = require('postcss-load-config')
-
-const getDependencyMessages = require('./lib/get-dependency-messages.js')
 
 const logo = `
                                /|\\
@@ -46,176 +45,262 @@ const version = () => {
 const argv = require('yargs')
   .usage(`${chalk.bold.red(logo)}\nUsage: \n\n$0 [--config|-c path/to/postcss.config.js] [input.css] [--output|-o output.css] [-watch|-w]`)
   .alias('e', 'env').describe('e', 'Environment')
+  .alias('x', 'ext').describe('x', 'Extension')
   .alias('c', 'config').describe('c', 'Config')
   .alias('o', 'output').describe('o', 'Output')
   .alias('d', 'dir').describe('d', 'Output Directory')
-  .alias('r', 'replace').describe('r', 'Replace the input file')
-  .alias('m', 'map').describe('m', 'Write sourcemap to external file')
-  .describe('no-map', 'Disable sourcemaps')
-  .alias('u', 'use').describe('u', 'List of plugins to apply').array('u')
+  .alias('r', 'replace').describe('r', 'Replace File')
+  .alias('m', 'map')
+    .describe('m', 'External Sourcemap')
+    .describe('no-map', 'Disable Sourcemaps')
+  .alias('u', 'use').describe('u', 'Plugin(s)').array('u')
   .alias('p', 'parser').describe('p', 'Parser')
   .alias('s', 'syntax').describe('s', 'Syntax')
   .alias('t', 'stringifier').describe('t', 'Stringifier')
-  .alias('w', 'watch').describe('w', 'Watch files for changes')
+  .alias('w', 'watch').describe('w', 'Watch')
   .help('h').alias('h', 'help')
   .version(version).alias('v', 'version')
   .requiresArg(['i', 'o'])
   .argv
 
+console.log(argv)
+
 let dir = argv.dir
+let ext = argv.ext
 let input = argv._
 let output = argv.output
 
-let config = {plugins: [], options: {}}
+let config = {
+  options: {
+    parser: argv.parser ? require(argv.parser) : undefined,
+    syntax: argv.syntax ? require(argv.syntax) : undefined,
+    stringifier: argv.stringifier ? require(argv.stringifier) : undefined
+  },
+  plugins: argv.use ? argv.use.map(plugin => require(plugin)) : []
+}
 
 if (argv.env) process.env.NODE_ENV = argv.env
 
 if (argv.config) argv.config = path.resolve(argv.config)
 else argv.config = process.cwd()
 
-if (!output && !dir && !argv.replace) {
-  throw new Error(`No Output specified, either --output, --dir, or --replace option must be passed`)
-}
+// if (!output && !dir && !argv.replace) {
+//   throw new Error(`No Output specified, either --output, --dir, or --replace option must be passed`)
+// }
 
-// Use warn to avoid writing to stdout
 console.warn(chalk.bold.red(logo))
 
-spinner.text = 'Loading Config'
-spinner.start()
-getConfig({}, argv.config)
+Promise.resolve()
   .then(() => {
-    spinner.succeed()
-
     if (input && input.length) return globber(input)
     // Else, read from stdin:
-    if (!argv.output) throw new Error('Must specify --output when reading from stdin')
-    if (argv.watch) throw new Error('Cannot run in watch mode when reading from stdin')
-    console.warn('No files passed, reading from stdin')
+
+    console.warn(chalk.bold.yellow('\n\nNo files passed, reading from stdin\n'))
+
+    if (argv.watch) {
+      throw new Error('Cannot run in watch mode when reading from stdin')
+    }
+
     return ['-']
   })
-  .then(expandedInput => {
-    input = expandedInput
-    if (!input || !input.length) throw new Error('You must pass a valid list of files to parse')
-    if (input.length > 1 && argv.output) throw new Error('Must use --dir or --replace with multiple input files')
+  .then((input) => {
+    if (!input || !input.length) {
+      throw new Error('You must pass a valid list of files to parse')
+    }
 
-    return processFiles(input)
+    if (input.length > 1 && argv.output) {
+      throw new Error('Must use --dir or --replace with multiple input files')
+    }
+
+    return input
   })
-  .then(function (results) {
+  .then((files) => {
+    if (typeof files === 'string') files = [ files ]
+
+    return Promise.all(files.map((file) => {
+      // Read from stdin
+      if (file === '-') {
+        return stdin()
+          .then((content) => css(content, 'stdin'))
+      }
+
+      return fs.readFile(file)
+        .then((content) => css(content, file))
+    }))
+  })
+  .then((results) => {
     if (argv.watch) {
-      spinner.text = 'Waiting for file changes...'
+      // console.warn('Waiting for file changes...')
 
       let watcher = chokidar
-      .watch(input.concat(getDependencyMessages(results)))
+        .watch(input.concat(dependencies(results)))
 
       watcher
-      .add(config.file)
-      .on('ready', (file) => spinner.start())
-      .on('change', (file) => {
+        .add(config.file)
+        .on('ready', (file) => console.warn('Waiting for file changes...'))
+        .on('change', (file) => {
         // If this is not a direct input file, process all:
-        if (input.indexOf(file) === -1) {
-          return getConfig()
-          .then(() => processFiles(input))
-          .then(results => watcher.add(getDependencyMessages(results)))
-          .catch(error)
-        }
+          if (input.indexOf(file) === -1) {
+            return files(input)
+              .then((results) => watcher.add(dependencies(results)))
+              .catch(error)
+          }
 
-        getConfig()
-        .then(() => processFiles(file))
-        .then(result => {
-          watcher.add(getDependencyMessages(result))
-          spinner.text = 'Waiting for file changes...'
-          spinner.start()
+          files(file)
+            .then((result) => watcher.add(dependencies(result)))
+            .catch(error)
         })
-        .catch(error)
-      })
     }
   })
   .catch(error)
 
-function processCSS (css, filename) {
-  var spinner = ora(`Processing ${filename || 'your CSS'}`)
-  spinner.start()
+function rc (ctx, path) {
+  if (argv.use) {
+    map()
+    return Promise.resolve(config)
+  }
 
-  let options = Object.assign(
-    {
-      from: filename,
-      to: output || (argv.replace ? filename : path.join(dir, path.basename(filename)))
-    },
-    config.options
-  )
-  options.to = path.resolve(options.to)
-
-  return postcss(config.plugins).process(css, options)
-    .then(result => {
-      if (result.messages.some(msg => msg.type === 'warning')) spinner.fail()
-
-      var tasks = [fs.outputFile(options.to, result.css)]
-      if (result.map) {
-        tasks.push(fs.outputFile(options.to.replace('.css', '.css.map'), result.map))
-      }
-      return Promise.all(tasks)
-        .then(() => {
-          spinner.succeed()
-          return result
-        })
+  return postcssrc(ctx, path)
+    .then((rc) => {
+      config = rc
+      map()
+      return config
     })
-}
-
-function processFiles (files) {
-  if (typeof files === 'string') files = [files]
-  return Promise.all(files.map(file => {
-    // Read from stdin
-    if (file === '-') {
-      return getStdin()
-      .then(css => processCSS(css))
-    }
-    return fs.readFile(file)
-    .then(css => processCSS(css, file))
-  }))
-}
-
-function getConfig (ctx, path) {
-  if (argv.use || argv.parser || argv.stringifier || argv.syntax) {
-    config = {
-      plugins: argv.use ? argv.use.map(plugin => require(plugin)) : [],
-      options: {
-        parser: argv.parser ? require(argv.parser) : undefined,
-        syntax: argv.syntax ? require(argv.syntax) : undefined,
-        stringifier: argv.stringifier ? require(argv.stringifier) : undefined
-      }
-    }
-    setMap()
-    return Promise.resolve()
-  } else {
-    return postcssrc(ctx, path)
-    .then(conf => {
-      config = conf
-      setMap()
-    })
-    .catch(err => {
+    .catch((err) => {
       if (err.message.indexOf('No PostCSS Config found') === -1) throw err
     })
-  }
 }
 
-function setMap () {
-  // Yargs interprets --no-map as argv.map: false:
+function map () {
   if (argv.map === false) config.options.map = false
-  // If --map is passed:
-  else if (argv.map) config.options.map = {inline: false}
-  // If neither --map or --no-map is passed:
+  else if (argv.map) config.options.map = { inline: false }
   else config.options.map = true
 }
 
-function error (err) {
-  try {
-    spinner.fail()
-  } catch (e) {
-    // Don't worry about this
+function files (files) {
+  if (typeof files === 'string') files = [ files ]
+
+  return Promise.all(files.map((file) => {
+    // Read from stdin
+    if (file === '-') {
+      return stdin()
+        .then((content) => css(content, 'stdin'))
+    }
+
+    return fs.readFile(file)
+      .then((content) => css(content, file))
+  }))
+}
+
+function css (css, file) {
+  const time = process.hrtime()
+  const spinner = ora(`Processing ${file}`)
+
+  spinner.start()
+
+  const ctx = { options: argv.ctx ? argv.ctx : {} }
+
+  if (file !== 'stdin') {
+    ctx.file = {
+      dirname: path.dirname(file),
+      extname: path.extname(file),
+      basename: path.basename(file)
+    }
+    if (argv.config === process.cwd()) argv.config = ctx.file.dirname
   }
+
+  rc(ctx, argv.config)
+    .then((config) => {
+      if (file !== 'stdin') {
+        config.options = Object.assign(
+          {
+            from: file,
+            to: output || (
+              argv.replace
+                ? file
+                : path.join(dir, path.basename(file))
+            )
+          },
+          config.options
+        )
+
+        config.options.to = path.resolve(config.options.to)
+
+        if (ext) {
+          config.options.to = config.options.to.replace(path.extname(file), ext)
+        }
+      }
+
+      return postcss(config.plugins)
+        .process(css, config.options)
+        .then((result) => {
+          if (result.messages) {
+            result.messages
+              .filter((msg) => msg.type === 'warning' ? msg : '')
+              .forEach((msg) => chalk.bold.yellow(msg.message))
+          }
+
+          if (output || dir) {
+            const results = [ fs.outputFile(config.options.to, result.css) ]
+
+            if (result.map) {
+              results.push(
+                fs.outputFile(
+                  config.options.to.replace('.css', '.css.map'), result.map
+                )
+              )
+            }
+
+            return Promise.all(results)
+              .then(() => {
+                console.log(results)
+                spinner.text = chalk.bold.green(
+                  `Finished ${file} (${process.hrtime(time)}s)`
+                )
+                spinner.succeed()
+                return result
+              })
+          }
+
+          const $ = new stream.Writable({
+            write: (chunk, enc, cb) => cb(chunk)
+          })
+
+          $.write(result.css)
+
+          if (result.map) $.write(result.map)
+
+          spinner.succeed()
+
+          return $.pipe(process.stdout)
+        })
+    })
+    .catch((err) => {
+      if (err.message.indexOf('No PostCSS Config found') === -1) throw err
+    })
+}
+
+function dependencies (results) {
+  if (!Array.isArray(results)) results = [ results ]
+
+  const messages = []
+
+  results
+    .forEach((result) => {
+      result.messages
+        .filter((msg) => msg.type === 'dependency' ? msg.file : '')
+        .forEach((dependency) => messages.push(dependency))
+    })
+
+  return messages
+}
+
+function error (err) {
   // Syntax Error
   if (err.name === 'CssSyntaxError') {
-    err.message = err.message.substr(err.file.length + 1).replace(/:\s/, '] ')
+    err.message = err.message
+      .substr(err.file.length + 1)
+      .replace(/:\s/, '] ')
 
     console.error(chalk.bold.red('\n', `[${err.message}`))
     console.error('\n', err.showSourceCode(), '\n')

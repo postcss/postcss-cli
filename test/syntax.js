88ff080535ed2fe3fe57a03a0749fe264d91245e
刘祺 <gucong@gmail.com>
2018-04-14 14:31:29 +0800

    automatically switch syntax

--- test/syntax.js
+++ test/syntax.js
@@ -20,3 +20,19 @@ test('--syntax works', async t => {
 
   t.is(await read(output), await read('test/fixtures/a.sss'))
 })
+
+test('automatically switch syntax', async t => {
+  const dir = tmp()
+
+  const { error, stderr } = await cli([
+    'test/fixtures/b.*',
+    '-d',
+    dir,
+    '--no-map'
+  ])
+
+  t.ifError(error, stderr)
+
+  t.is(await read(`${dir}/b.sss`), await read('test/fixtures/b.sss'))
+  t.is(await read(`${dir}/b.css`), await read('test/fixtures/b.css'))
+})

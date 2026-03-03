0c9da903b4bee06aee920fe573bf7c36b615207e
Rolando Bosch <rbosch@lpci.ai>
2026-03-03 10:07:14 -0800

    refactor: convert env helper to async/await per review feedback

--- test/helpers/env.js
+++ test/helpers/env.js
@@ -4,21 +4,21 @@ import { glob } from 'tinyglobby'
 
 import tmp from './tmp.js'
 
-export default function (config, fixtures = '**/*', extension = 'cjs') {
+export default async function (config, fixtures = '**/*', extension = 'cjs') {
   const dir = tmp()
 
-  return fs.mkdir(dir, { recursive: true }).then(() =>
-    Promise.all([
-      glob(fixtures, { cwd: 'test/fixtures' }).then((list) => {
-        return Promise.all(
-          list.map(async (item) => {
-            const dest = path.join(dir, item)
-            await fs.mkdir(path.dirname(dest), { recursive: true })
-            return fs.copyFile(path.join('test/fixtures', item), dest)
-          }),
-        )
-      }),
-      fs.writeFile(path.join(dir, `postcss.config.${extension}`), config),
-    ]).then(() => dir),
-  )
+  await fs.mkdir(dir, { recursive: true })
+
+  const list = await glob(fixtures, { cwd: 'test/fixtures' })
+
+  await Promise.all([
+    ...list.map(async (item) => {
+      const dest = path.join(dir, item)
+      await fs.mkdir(path.dirname(dest), { recursive: true })
+      await fs.copyFile(path.join('test/fixtures', item), dest)
+    }),
+    fs.writeFile(path.join(dir, `postcss.config.${extension}`), config),
+  ])
+
+  return dir
 }

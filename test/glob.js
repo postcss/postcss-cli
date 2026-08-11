35ae0858987436644d20bb58a9a67d951615909c
Ryan Zimmerman <opensrc@ryanzim.com>
2026-08-11 16:03:21 -0400

    Add test for directory as input (#521)

--- test/glob.js
+++ test/glob.js
@@ -30,3 +30,29 @@ test('works with glob patterns', async (t) => {
     await read('test/fixtures/glob/s.css'),
   )
 })
+
+test('works with directory input', async (t) => {
+  const output = tmp()
+
+  const { error, stderr } = await cli([
+    'test/fixtures/glob',
+    '-d',
+    output,
+    '--no-map',
+  ])
+
+  t.falsy(error, stderr)
+
+  t.is(
+    await read(path.join(output, 'a.css')),
+    await read('test/fixtures/glob/a.css'),
+  )
+  t.is(
+    await read(path.join(output, 'b.css')),
+    await read('test/fixtures/glob/b.css'),
+  )
+  t.is(
+    await read(path.join(output, 's.css')),
+    await read('test/fixtures/glob/s.css'),
+  )
+})
